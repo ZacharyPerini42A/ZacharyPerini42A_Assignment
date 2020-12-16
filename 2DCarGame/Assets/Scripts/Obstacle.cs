8b97260c7c8e7548ed877594d89ce5e9a5e38eea
Perinski0411 <73109614+Perinski0411@users.noreply.github.com>
2020-12-16 16:17:59 +0100

    Added Collisions between Obstacles and player

---
+++ 2DCarGame/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+
+    //reduces health whenever enemy collides with a gameObject which has DamageDealer component
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        //access the Damage Dealer from the "other" object which hit the enemy
+        //and depending on the damage reduce health
+
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        health -= damageDealer.GetDamage();
+        
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
