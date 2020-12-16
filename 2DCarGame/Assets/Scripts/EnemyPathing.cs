32229d555d09d212b5c565abbdd49bcb1c3fd566
Perinski0411 <73109614+Perinski0411@users.noreply.github.com>
2020-12-16 14:45:06 +0100

    Created paths and waves for 2 obstacles

---
+++ 2DCarGame/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] WaveConfig waveConfig;
+    int waypointIndex = 0;
+
+    void EnemyMove()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            //save the current waypoint in targetPosition
+            //targetPosition: where we want to go
+            var targetPosition = waypoints[waypointIndex].transform.position;
+
+            //to make sure z position is 0	
+            targetPosition.z = 0f;
+
+            var movementThisFrame =  waveConfig.GetEnemyMoveSpeed() * Time.deltaTime;
+            //move from the current position, to the target position, the maximum distance one can move
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            //if we reached the target waypoint
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        //if enemy moved to all waypoints
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+    }
+}
