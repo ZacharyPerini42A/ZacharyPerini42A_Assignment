adbe2a5ca1aaa3f36bd98c3215598851d9fe608f
Perinski0411 <73109614+Perinski0411@users.noreply.github.com>
2020-12-17 11:44:03 +0100

    Added damage dealer class to obstacles and deals damage to player

--- 2DCarGame/Assets/Scripts/PlayerCar.cs
+++ 2DCarGame/Assets/Scripts/PlayerCar.cs
@@ -7,6 +7,7 @@ public class PlayerCar : MonoBehaviour
 
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 1f;
+    [SerializeField] float health = 100f;
 
 
     float xMin, xMax;
@@ -41,5 +42,30 @@ public class PlayerCar : MonoBehaviour
     void Update()
     {
         Move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        //access the damage dealer class from "otherobject which hits enemy and reduces health accordingly"
+        DamageDealer dmgDealer = otherObject.gameObject.GetComponent<DamageDealer>();
+
+        if (!dmgDealer)//if dmgDealer == null
+        {
+            return;
+        }
+
+        ProcessHit(dmgDealer);
+    }
+
+    //whenever this is called send us the damagedealer details
+    private void ProcessHit(DamageDealer dmgDealer)
+    {
+        health -= dmgDealer.GetDamage();
+        //destroy laser
+        dmgDealer.Hit();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
